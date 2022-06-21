254f440e7c4cfcae1686d4403295db2f7003a61f
Alex Beregszaszi <alex@rtfs.hu>
2022-06-21 11:37:15 +0200

    Drop dead code

--- crates/capi/src/lib.rs
+++ crates/capi/src/lib.rs
@@ -8,38 +8,6 @@ mod sys;
 
 use sys::*;
 
-/*
-fn allocate_output_data(output: Option<&Vec<u8>>) -> (*const u8, usize) {
-    if let Some(buf) = output {
-        let buf_len = buf.len();
-
-        // Manually allocate heap memory for the new home of the output buffer.
-        let memlayout = std::alloc::Layout::from_size_align(buf_len, 1).expect("Bad layout");
-        let new_buf = unsafe { std::alloc::alloc(memlayout) };
-        unsafe {
-            // Copy the data into the allocated buffer.
-            std::ptr::copy(buf.as_ptr(), new_buf, buf_len);
-        }
-
-        (new_buf as *const u8, buf_len)
-    } else {
-        (std::ptr::null(), 0)
-    }
-}
-
-unsafe fn deallocate_output_data(ptr: *const u8, size: usize) {
-    if !ptr.is_null() {
-        let buf_layout = std::alloc::Layout::from_size_align(size, 1).expect("Bad layout");
-        std::alloc::dealloc(ptr as *mut u8, buf_layout);
-    }
-}
-
-
-
-int32_t identity_exec(const uint8_t* input, uint32_t input_size, uint8_t* output,
-    [[maybe_unused]] uint32_t output_size) noexcept
-*/
-
 fn error_to_c(input: Error) -> i32 {
     match input {
         Error::InvalidInput => evmc_status_code::INVALID_INPUT as i32,
